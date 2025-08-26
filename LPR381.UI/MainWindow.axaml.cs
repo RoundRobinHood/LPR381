95394e7aa6e54ed4456ba2d0d99f11b1231ead16
CL-Frank <142659168+CL-Frank@users.noreply.github.com>
2025-08-26 18:23:17 +0200

    Revert "Merge branch 'UI-branch' into Core-UI-Intergration"
    This reverts commit b66c69645718a49c7d8b76fcdf7e11e30cb29f55, reversing
    changes made to 54efac1ac66503ab2638375bc7bf95678a8bfbed.

--- LPR381.UI/MainWindow.axaml.cs
+++ LPR381.UI/MainWindow.axaml.cs
@@ -1,7 +1,4 @@
-using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Interactivity;
-using Avalonia.Layout;
 using LPR381.Core;
 
 namespace LPR381.UI;
@@ -11,67 +8,9 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+
+        int result = Logic.add(3, 4);
+
+        this.Title = $"3 + 4 = {result}";
     }
-    //Add Constraints Button Functionality
-    private void AddConstraintButton_Click(object sender, RoutedEventArgs e)
-    {
-        // Create a new row
-        var row = new StackPanel
-        {
-            Orientation = Orientation.Horizontal,
-            Margin = new Thickness(0, 5, 0, 0)
-        };
-
-        // Coefficients textbox
-        var coeffBox = new TextBox
-        {
-            Width = 250,
-            Watermark = "Coefficients e.g. 2x1 + 3x2"
-        };
-
-        // Operator dropdown
-        var combo = new ComboBox
-        {
-            Width = 70,
-            Margin = new Thickness(5, 0, 0, 0),
-            Items =
-                {
-                    new ComboBoxItem { Content = "<=" },
-                    new ComboBoxItem { Content = ">=" },
-                    new ComboBoxItem { Content = "=" }
-                }
-        };
-
-        // RHS textbox
-        var rhsBox = new TextBox
-        {
-            Width = 80,
-            Watermark = "RHS",
-            Margin = new Thickness(5, 0, 0, 0),
-        };
-        // Delete button
-        var deleteBtn = new Button
-        {
-            Content = "Delete",    // small cross
-            Width = 110,
-            Height = 30,
-            Margin = new Thickness(5, 0, 0, 0)
-        };
-
-        // delete functionality
-        deleteBtn.Click += (s, args) =>
-        {
-            ConstraintsPanel.Children.Remove(row);
-        };
-
-        // Add them to row
-        row.Children.Add(coeffBox);
-        row.Children.Add(combo);
-        row.Children.Add(rhsBox);
-        row.Children.Add(deleteBtn);
-
-        // Add row to panel
-        ConstraintsPanel.Children.Add(row);
-    }
-
 }
